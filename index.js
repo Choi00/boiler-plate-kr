29c2f861f1c5270000891d7294daacbd72388ead
choi00 <gg561400@naver.com>
2020-12-06 18:10:34 +0900

    비밀 정보 보호

--- index.js
+++ index.js
@@ -1,7 +1,13 @@
 const express = require('express')
 const app = express()
 const port = 5000
+const bodyParser = require('body-parser');
+const { User } = require("./models/User");
 
+// application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({extended: true}));
+// application/json
+app.use(bodyParser.json());
 
 const mongoose = require('mongoose')
 mongoose.connect('mongodb+srv://admin:0000@youtubeclone.69vhv.mongodb.net/Youtubeclone?retryWrites=true&w=majority', {
@@ -11,6 +17,22 @@ mongoose.connect('mongodb+srv://admin:0000@youtubeclone.69vhv.mongodb.net/Youtub
 
 
 
-app.get('/', (req, res) => res.send('Hello World!ㄱㄱㄱㄱ'))
+app.get('/', (req, res) => res.send('Hello World!'))
+
+app.post('/register', (req, res) => {
+  // 회원 가입 할 때 필요한 정보들을 client 에서 가져오면
+  // 그것들을 데이터 베이스에 넣어준다.
+
+  
+
+      const user = new User(req.body)
+
+      user.save((err, userInfo) => {
+        if(err) return res.json({ success: false, err})
+        return res.status(200).json({
+          success: true
+        })
+      })
+})
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
